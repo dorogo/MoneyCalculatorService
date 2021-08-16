a5eaaf8a917196df8d5955cfc3b84d9f0f81e33a
dorogo <ill.shir@gmail.com>
2021-08-17 01:03:32 +0300

    Initial commit

---
+++ src/main/java/com/dorogo/MoneyCalculatorService/MoneyCalculatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dorogo.MoneyCalculatorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyCalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyCalculatorServiceApplication.class, args);
+	}
+
+}
